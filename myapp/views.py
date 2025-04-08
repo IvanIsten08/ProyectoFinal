f42aad57da4830173ee7a74f642315e64860db00
Ivan Gimenez <ivanlgimenez@hotmail.com>
2025-04-08 00:58:26 -0300

    Add initial Django app structure and basic views
    - Updated settings to include 'myapp'.
    - Created URL routing for the app.
    - Added base, index, and about templates.
    - Implemented index and about views.
    - Included navigation and footer components.

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    context = {'msj': 'Comienza ahora a publicar tu blog'}
+    
+    return render(request, 'myapp/index.html', context)
+
+def about(request):
+    
+    return render(request, 'myapp/about.html')
